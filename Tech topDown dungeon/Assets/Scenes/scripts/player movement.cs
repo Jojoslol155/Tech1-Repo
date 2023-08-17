b6db894272c6a478b89c9d2b8d25f1941b6059e9
Jojoslol155 <bajorge9@gmail.com>
2023-08-17 12:43:20 -0700

    Added PlayerMovement

--- Tech topDown dungeon/Assets/Scenes/scripts/player movement.cs
+++ Tech topDown dungeon/Assets/Scenes/scripts/player movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
+    public float speed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,11 @@ public class playermovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxisRaw("Horizontal"); //X input
+        float y = Input.GetAxisRaw("Vertical");  //Y input
+        Vector2 direction = new Vector2(x, y); //Direction we move
+        transform.Translate(direction * Time.deltaTime * speed); //How we move
     }
+
+
 }
