96183ea17b03c921e564d83cb6899d531c74039b
Jojoslol <Bajorge9@gmail.com>
2023-09-26 00:04:32 -0700

    SpawnPoint first step

---
+++ Tech topDown dungeon/Assets/Scenes/scripts/RoomSpawner.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 --> need bottom door
+    // 2 --> need top door
+}
